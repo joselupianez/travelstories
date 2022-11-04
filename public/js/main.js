11a6fbafd59801e35170862721c0c3494709771f
Jose Lupianez <joselupianez@fullsail.edu>
2022-11-04 09:07:48 -0400

    Add new style

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,7 @@
-const alertButton = document.querySelectorAll('.alert-container').forEach(a => {
+const alertButton = document.querySelectorAll('.alert').forEach(a => {
     a.querySelector('.alert-button').addEventListener('click', () => {
         a.remove()
     })
-})
-
-const mobileButton = document.querySelector('#menu-btn');
-const nav = document.querySelector('#menu');
-
-mobileButton.addEventListener('click', () => {
-    mobileButton.classList.toggle('open')
-    nav.classList.toggle('flex')
-    nav.classList.toggle('hidden')
 })
 
 CKEDITOR.replace('body', {
